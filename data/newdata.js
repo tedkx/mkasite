54ad857d6b38897ce8b1b8387657128e04294f8b
Tedk <thodwriskalaitzidis@hotmail.com>
2020-03-23 20:36:07 +0200

    Added money-shot and image-sections2 templates

--- data/newdata.js
+++ data/newdata.js
@@ -1,15 +1,133 @@
 window.sitedata = {
-    "projects": {
-        "v1": {
+    "projects": [
+        {
             "backColor": "#27282C",
-            "foreColor": "#FFF",
-            "title": "Κτίριο Γραφείων με Ισόγειο Κατάστημα στην οδό Πέτρου Ράλλη",
-            "template": "parallax1",
-            "images": ["/img/projects/v1/2D.png", "/img/projects/v1/papadopoulos.621.png", '/img/projects/v1/papadopoulos.627PH.png'],
             "content": [
                 "Το κτίσμα φιλοξενεί 3 επίπεδα γραφείων στους ορόφους, ενώ στο ισόγειο υπάρχει κατάστημα με υπόγειο. Η συνολική επιφάνειά τους είναι E=522μ2. Στο δώμα υπάρχει ένα μικρό διαμέρισμα κατοικίας.",
-                "Το μικρό οικόπεδο και οι πολεοδομικοί κανονισμοί σε συνδιασμό με την επιθυμία του ιδιοκτήτη για εκμετάλλευση δεν επιτρέπουν 'παιχνίδι με τους όγκους' κι έτσι η όψη έχει προκύψει σχετικά επίπεδη. Η έλλειψη εξωστών αποτελεί σχεδιαστική επιλογή μας και σε κτίρια γραφείων σπανίως χρησιμοποιούνται. Τα μεγάλα τετράγωνα ανοίγματα στην δυτική όψη φωτίζουν τους εσωτερικούς χώρους ενώ τα μικρότερα και γραμμικά από τη βορεινή προστατεύουν την κατασκευή από ενεργειακές απώλειες και άρα κάνουν την κατασκευή οικονομικότερη στη λειτουργία και πιο φιλική στο περιβάλλον."
+            ],
+            "foreColor": "#FFF",
+            "id": "v1",
+            "images": ["/img/projects/v1/2D.png", "/img/projects/v1/papadopoulos.621.png", '/img/projects/v1/papadopoulos.627PH.png'],
+            "shortTitle": "Ισόγειο Π. Ράλλη",
+            "template": "image-sections1",
+            "thumbnail": "1.png",
+            "title": "Κτίριο Γραφείων με Ισόγειο Κατάστημα στην οδό Πέτρου Ράλλη",
+            "sections": [
+                { 
+                    "image": "/img/projects/v1/2D.png",
+                    "caption": "",
+                    "captionPosition": "top right"
+                },
+                { 
+                    dark: true,
+                    "image": "/img/projects/v1/papadopoulos.621.png",
+                    "caption": "Το μικρό οικόπεδο και οι πολεοδομικοί κανονισμοί σε συνδιασμό με την επιθυμία του ιδιοκτήτη για εκμετάλλευση δεν επιτρέπουν 'παιχνίδι με τους όγκους' κι έτσι η όψη έχει προκύψει σχετικά επίπεδη. Η έλλειψη εξωστών αποτελεί σχεδιαστική επιλογή μας και σε κτίρια γραφείων σπανίως χρησιμοποιούνται",
+                    "captionPosition": "top right"
+                },
+                { 
+                    dark: true,
+                    "image": "/img/projects/v1/papadopoulos.627PH.png",
+                    "caption": "Τα μεγάλα τετράγωνα ανοίγματα στην δυτική όψη φωτίζουν τους εσωτερικούς χώρους ενώ τα μικρότερα και γραμμικά από τη βορεινή προστατεύουν την κατασκευή από ενεργειακές απώλειες και άρα κάνουν την κατασκευή οικονομικότερη στη λειτουργία και πιο φιλική στο περιβάλλον",
+                    "captionPosition": "bottom left"
+                },
             ]
         },
+        {
+            "description": "TEXT",
+            "heroImage": "oinofyta_img_money_shot.jpg",
+            "id": "rubik",
+            "images": [
+                { 
+                    img: 'on_front.jpg',
+                    thumb: 'on_front.jpg'
+                },
+                { 
+                    img: 'on_side.jpg',
+                    thumb: 'on_side.jpg'
+                },
+                { 
+                    img: 'on_top.jpg',
+                    thumb: 'on_top.jpg'
+                },
+                { 
+                    img: 'on_perspectice.jpg',
+                    thumb: 'on_perspectice.jpg'
+                },
+                { 
+                    img: 'oinofyta_img_3.jpg',
+                    thumb: 'oinofyta_img_3_thumb.jpg'
+                },
+                { 
+                    img: 'oinofyta_plai_with_sunr.jpg',
+                    thumb: 'oinofyta_plai_with_sunr_thumb.jpg'
+                }
+            ],
+            "shortTitle": "Πάρκο Ρούμπικ",
+            "template": "money-shot1",
+            "thumbnail": "2.png",
+            "title": "Πάρκο Ρούμπικ"
+        },{
+            "id": "3",
+            "shortTitle": "Στα Βουνά",
+            "thumbnail": "3.png",
+            "title": "Στα Βουνά"
+        },{
+            "backColor": "#27282C",
+            "foreColor": "#FFF",
+            "id": "v1-2",
+            "shortTitle": "Ισόγειο Π. Ράλλη Take 2",
+            "template": "image-sections2",
+            "thumbnail": "4.png",
+            "title": "Κτίριο Γραφείων με Ισόγειο Κατάστημα στην οδό Πέτρου Ράλλη",
+            "sections": [
+                { "caption": "Το κτίσμα φιλοξενεί 3 επίπεδα γραφείων στους ορόφους, ενώ στο ισόγειο υπάρχει κατάστημα με υπόγειο. Η συνολική επιφάνειά τους είναι E=522μ2. Στο δώμα υπάρχει ένα μικρό διαμέρισμα κατοικίας." },
+                { "image": "/img/projects/v1/2D.png" },
+                { "caption": "Το μικρό οικόπεδο και οι πολεοδομικοί κανονισμοί σε συνδιασμό με την επιθυμία του ιδιοκτήτη για εκμετάλλευση δεν επιτρέπουν 'παιχνίδι με τους όγκους' κι έτσι η όψη έχει προκύψει σχετικά επίπεδη. Η έλλειψη εξωστών αποτελεί σχεδιαστική επιλογή μας και σε κτίρια γραφείων σπανίως χρησιμοποιούνται" },
+                { "image": "/img/projects/v1/papadopoulos.621.png" },
+                { "caption": "Τα μεγάλα τετράγωνα ανοίγματα στην δυτική όψη φωτίζουν τους εσωτερικούς χώρους ενώ τα μικρότερα και γραμμικά από τη βορεινή προστατεύουν την κατασκευή από ενεργειακές απώλειες και άρα κάνουν την κατασκευή οικονομικότερη στη λειτουργία και πιο φιλική στο περιβάλλον" },
+                { "image": "/img/projects/v1/papadopoulos.627PH.png" },
+            ]
+        },{
+            "id": "5",
+            "shortTitle": "Ασπρόμαυρο #2",
+            "thumbnail": "5.png",
+            "title": "Ασπρόμαυρο #2"
+        },{
+            "id": "6",
+            "shortTitle": "Ασπρόμαυρο #3",
+            "thumbnail": "6.png",
+            "title": "Ασπρόμαυρο #3"
+        },{
+            "id": "7",
+            "shortTitle": "Ασπρόμαυρο #4",
+            "thumbnail": "7.png",
+            "title": "Ασπρόμαυρο #4"
+        },{
+            "id": "8",
+            "shortTitle": "Ασπρόμαυρο #5",
+            "thumbnail": "8.png",
+            "title": "Ασπρόμαυρο #5"
+        },{
+            "id": "9",
+            "shortTitle": "Ασπρόμαυρο #6",
+            "thumbnail": "9.png",
+            "title": "Ασπρόμαυρο #6"
+        }
+    ],
+    "templates": {
+        "image-sections1": {
+            "hero": "<div class=\"hero\"><img src=\"/img/projects/v1/papadopoulos.625PH-gradient.png\" width=\"100%\" /><h1 id=\"title\">{title}</h1></div>",
+            "section-template": "<div class=\"section\"><img src=\"{image}\" /><div class=\"caption {position}\">{caption}</div></div>",
+            "section-no-caption-template": "<div class=\"section\"><img src=\"{image}\" /></div>"
+        },
+        "image-sections2": {
+            "hero": "<div class=\"hero\"><img src=\"/img/projects/v1/papadopoulos.625PH-gradient.png\" width=\"100%\" /><h1 id=\"title\">{title}</h1></div>",
+            "image-section": "<div class=\"section\"><img src=\"{image}\" /></div>",
+            "caption-section": "<div class=\"section caption\">{caption}</div>"
+        },
+        'money-shot1': {
+            "hero": "<div class=\"hero\" style=\"background-image: url('{heroImage}');\"></div>",
+            "imageContainer": "<div class=\"image-link\" data-src=\"{image}\"><img src=\"{thumbnail}\" /></div>"
+        }
     }
 }
--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@ function noop() {}
 
 $(document).ready(function () {
     // Configuration
-    var PROJECTS_TRANSITION_OUT_DURATION = 500,
+    var COLORIZED_SELECTOR = '#project .hero,#project-spacer,#project-template>*:not(.parallax-container)',
+        PROJECTS_TRANSITION_OUT_DURATION = 500,
         PROJECTS_TRANSITION_OUT_DELAY = 50,
         TITLE_SUFFIX = ' | MKA';
 
-    var thumbs = Array.from(Array(9)).map((_, i) => ({
-        id: i === 0 ? 'v1' : (i + 1) + '',
-        img: `/img/thumbnails/${i + 1}.png`
-    }));
-
     var projects = window.sitedata.projects;
+    var templateData = window.sitedata.templates;
+    delete window.sitedata;
 
     var isotopeOpts = {
         percentPosition: true,
@@ -30,66 +28,119 @@ $(document).ready(function () {
     var $content = $('#content'),
         $list = $('#projects-list'),
         $project = $('#project'),
-        $colorized = $('#project>*:not(.parallax-container)'),
+        $projectTemplate = $project.find('#project-template'),
+        $previousProject = $project.find('#previous-project'),
+        $projectsToList = $project.find('#project-tolist'),
+        $nextProject = $project.find('#next-project'),
+        $previousProjectTitle = $project.find('#previous-project-title'),
+        $nextProjectTitle = $project.find('#next-project-title'),
         $projectTitle = $project.find('#title'),
         $projectSections = $project.find('.section'),
         selectedProject = null;
 
     function resizeContent() { 
         $content.css('min-height', $(window).height());
-        var parallaxHeight = Math.max($(window).height() * 0.9, 200) | 0;
-        $('.parallax-container').height(parallaxHeight);
+        //var parallaxHeight = Math.max($(window).height() * 0.9, 200) | 0;
+        //$('.parallax-container').height(parallaxHeight);
+    }
+
+    function preventor(e) {
+        if(e && e.preventDefault) e.preventDefault();
+        return false;
+    }
+
+    function getImageUrl(url, project) {
+        return '/img/projects/' + project.id + '/' + url;
+    }
+
+    function scrollToTop(e) {
+        if(e && e.preventDefault) e.preventDefault();
+        if($content[0].scrollIntoView)
+            $content[0].scrollIntoView({ block: 'start', behavior: 'smooth' });
+        return false;
     }
 
     /* Event Handlers */
     function onRouteChange(e) {
-        console.log('history state changed', e.state, typeof e.state, (e.state + ''));
-        if(e.state === 'home') {
-            console.log('going to home');
-            switchToProjectsList();
-        } else {
-            var projectId = e.state.replace('project_');
-            console.log('switching to project', projectId);
+        try {
+            if(e.state === 'home')
+                switchToProjectsList();
+            else
+                var projectId = e.state.replace('project_');
+        } catch(ex) {
+            console.log('route change exception', ex);
         }
+    }
+    
+    function cleanupProject() {
+        if(!selectedProject) return;
+        $(COLORIZED_SELECTOR).css('background-color', '#FFF').css('color', '#000');
+        $project.removeClass(selectedProject.template);
+        var callbacks = templateCallbacks[selectedProject.template];
+        if(callbacks && callbacks.cleanup) callbacks.cleanup(selectedProject);
     }
 
     function onProjectSelect(e) {
         e.stopPropagation();
         switchToProject($(e.currentTarget).data('id'));
     }
+    function onPrevNext(e) {
+        e.stopPropagation();
+        switchToProject($(e.currentTarget).data('id'));
+    }
     
     /* Section Switchers */
     function switchToProject(id) {
+        $project.removeClass('visible');
+        scrollToTop();
+        setTimeout(cleanupProject, 500);
+
+        var switchDelay = !!selectedProject ? 500 : 100;
+
         // hide projects list if a project is not already selected
         if(!selectedProject) {
             $list.addClass('transition-out');
             setTimeout(function () {
                 $list.addClass('dormant').removeClass('visible').removeClass('transition-out');
-            }, PROJECTS_TRANSITION_OUT_DURATION + PROJECTS_TRANSITION_OUT_DELAY * thumbs.length);
+            }, PROJECTS_TRANSITION_OUT_DURATION + PROJECTS_TRANSITION_OUT_DELAY * projects.length);
         }
 
         // show project data
         setTimeout(function() {
-            selectedProject = projects[id] || { color: '#FFF', id: id, title: 'Πρότζεχτ' };
+            for(var i = 0; i < projects.length; i++) {
+                if(projects[i].id == id) {
+                    selectedProject = projects[i];
+                    var previousProject = projects[i === 0 ? projects.length - 1 : i - 1];
+                    $previousProjectTitle.html(previousProject.shortTitle);
+                    $previousProject.data('id', previousProject.id);
 
+                    var nextProject = projects[i >= projects.length - 1 ? 0 : i + 1];
+                    $nextProjectTitle.html(nextProject.shortTitle);
+                    $nextProject.data('id', nextProject.id);
+                }
+            }
+            selectedProject = projects.find(p => p.id == id) || { color: '#FFF', id: id, title: 'Πρότζεχτ' };
             window.history.pushState('project_' + id, selectedProject.title + TITLE_SUFFIX, '/projects/' + id);
 
-            $colorized.css('background-color', selectedProject.backColor).css('color', selectedProject.foreColor);
+            //console.log('switching to project', id, selectedProject);
 
-            if(callbacks[id])
-                callbacks[id](selectedProject);
+            var initializer = templateCallbacks[selectedProject.template] || templateCallbacks.default;
+            initializer.init(selectedProject, templateData[selectedProject.template]);
 
+            $(COLORIZED_SELECTOR).css('background-color', selectedProject.backColor).css('color', selectedProject.foreColor);
             $project.addClass(selectedProject.template);
 
-            setTimeout(function () { $project.addClass('visible'); }, 100);
-        }, !selectedProject ? PROJECTS_TRANSITION_OUT_DURATION : 0);
+            setTimeout(function () { $project.removeClass('dormant').addClass('visible'); }, 100);
+        }, !selectedProject ? PROJECTS_TRANSITION_OUT_DURATION : 500);
     }
 
     function switchToProjectsList(ev) {
+        cleanupProject();
+        $project.addClass('dormant');
         if(selectedProject) {
             history.pushState('home', 'Αρχική' + TITLE_SUFFIX, '/');
             $project.removeClass('visible');
-            $colorized.css('background-color', '#FFF');
+            $(COLORIZED_SELECTOR).css('background-color', '#FFF').css('color', '#000');
             $list.removeClass('dormant');
         }
         selectedProject = null;
@@ -97,44 +148,141 @@ $(document).ready(function () {
         setTimeout(function () { $list.addClass('visible'); }, ev && ev.isComplete === true ? 350 : 1);
     }
 
-     /* Per project callbacks */
-     var callbacks = {
-        v1: function(project) {
-            $projectTitle.html(project.title);
+     /* Per template callbacks */
+     var templateCallbacks = {
+         'default': {
+            init: function() {
+                $projectTemplate.html('')
+            }
+         },
+        'image-sections1': {
+            init: function(project, template) {
+                var str = (project.sections.map(function(section) {
+                    var sectionTemplate = section.caption
+                        ? template['section-template']
+                        : template['section-no-caption-template'];
+                    return sectionTemplate
+                        .replace('{image}', section.image)
+                        .replace('{position}', section.captionPosition)
+                        .replace('{caption}', section.caption);
+                }).join(''));
 
-            var $parallaxContainers = $('.parallax-container');
-            for(var i = 0; i < project.images.length; i++)
-                $parallaxContainers.eq(i).parallax({ mirrorContainer: $project, imageSrc: project.images[i] });
+                $projectTemplate.html(template.hero.replace('{title}', project.title) + 
+                    '<div class="description">' + 
+                    project.content[0] + '</div>' + str);
 
-            for(var i = 0; i < project.content.length; i++)
-                $projectSections.eq(i).html(project.content[i]);
-            setTimeout(function() { $(window).trigger('resize'); }, 150);
+                window.waypoints = [];
+                setTimeout(function() {
+                    $projectTemplate.find('.caption').each(function(_, elem) {
+                        window.waypoints.push(new Waypoint({
+                            element: elem,
+                            handler: function(direction) {
+                                $(elem).addClass('reached');
+                            },
+                            offset: '80%'
+                        }))
+                    })
+                }, 500);                
+            },
+            cleanup: function(project) {
+                Waypoint.destroyAll();
+            },
+        },
+        'image-sections2': {
+            init: function(project, template) {
+                $project.addClass('image-sections2');
+                var str = (project.sections.map(function(section) {
+                    return section.image
+                        ? template['image-section'].replace('{image}', section.image)
+                        : template['caption-section'].replace('{caption}', section.caption);
+                }).join(''));
+
+                $projectTemplate.html(template.hero.replace('{title}', project.title) + str);
+            },
+            cleanup: function(project) { }
+        },
+        'money-shot1': {
+            init: function(project, template) {
+                $projectTitle.html(project.title);
+                var getImageElem = function(imageObj) {
+                    return '<div class="block image-block">' +
+                        template.imageContainer
+                        .replace('{image}', getImageUrl(imageObj.img, project))
+                        .replace('{thumbnail}', getImageUrl(imageObj.thumb, project)) +
+                        '</div>';
+                }
+                
+                $projectTemplate.html(template.hero
+                        .replace('{heroImage}', getImageUrl(project.heroImage, project)) +
+                    '<div class="block" style="margin: 25px 0">' + 
+                        '<div class="row">' +
+                            '<div class="block">' +
+                                '<div class="row">' + project.images.slice(0, 2).map(getImageElem).join('') + '</div>' +
+                                '<div class="row">' + project.images.slice(2, 4).map(getImageElem).join('') + '</div>' +
+                            '</div>' +
+                            '<div class="block description">' + (project.description || '') + '</div>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="row">' + project.images.slice(4, 6).map(getImageElem).join('') + '</div>'
+                );
+
+                $projectTemplate.find('.block .row .image-block').addClass('small');
+
+                setTimeout(function() {
+                    $project.lightGallery({
+                        selector: '.image-link'
+                    });
+                }, 500);
+            },
+            cleanup: function(project) { }
+        },
+        'parallax1': {
+            init: function(project, template) {
+                $projectTitle.html(project.title);
+
+                var $parallaxContainers = $('.parallax-container');
+                
+                for(var i = 0; i < project.images.length; i++)
+                    $parallaxContainers.eq(i).html(`<img src="${project.images[i]}" />`);
+                    //$parallaxContainers.eq(i).parallax({ mirrorContainer: $project, imageSrc: project.images[i] });
+
+                for(var i = 0; i < project.content.length; i++)
+                    $projectSections.eq(i).html(project.content[i]);
+                setTimeout(function() { $(window).trigger('resize'); }, 150);
+            },
+            cleanup: function(project) { }
         }
     }
 
     /* Initialization */
 
     $list.html(`<div class="projects-sizer"></div>
-            ${thumbs.map(function (p) {
+            ${projects.map(function(p) {
         return `
                 <div class="project" data-id=${p.id}>
                     <div class="overlay">
-                        <span>Project #${p.id}</span>
+                        <span>${p.shortTitle}</span>
                     </div>
-                    <img src="${p.img}" />
+                    <img src="/img/thumbnails/${p.thumbnail}" />
                 </div>
             `}).join('')}`);
 
     $list.on('click', '.project', onProjectSelect);
+    $previousProject.on('click', onPrevNext);
+    $nextProject.on('click', onPrevNext);
+    $projectsToList.on('click', switchToProjectsList);
 
     $list.isotope(isotopeOpts);
 
-    $list.imagesLoaded().always(switchToProjectsList);
-    //$list.imagesLoaded().always(() => switchToProject('v1'));
+    var urlMatch = window.location.href.match(/\/projects\/(.+)/);
+    if(urlMatch)
+        $list.imagesLoaded().always(() => switchToProject(urlMatch[1]));
+    else
+        $list.imagesLoaded().always(switchToProjectsList);
 
     resizeContent();
-    $(window).on('click', function (e) {
-        if (selectedProject) 
-            switchToProjectsList();
-    }).on('resize', resizeContent);
+    // .on('click', function (e) {
+    //     if (selectedProject) 
+    //         switchToProjectsList();
+    $(window).on('resize', resizeContent);
 })
